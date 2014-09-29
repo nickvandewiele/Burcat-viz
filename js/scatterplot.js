4ba55f37d0a452376e2122a6d0c84fb4f8b7c616
Nick Vandewiele <nickvandewiele@gmail.com>
2014-09-29 10:24:20 -0400

    add circle text labels on hover using CSS selector

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -6,20 +6,20 @@ function createScatterPlot(dataset) {
 	var padding = 30;
 
 	var xScale = d3.scale.linear().domain([d3.min(dataset, function(d) {
-		var hf_entry = d3.select(d).select('hf298_1')[0][0];
+		var hf_entry = select_hf298_1(d);
 		return parse(hf_entry)[0];
 	}), d3.max(dataset, function(d) {
-		var hf_entry = d3.select(d).select('hf298_1')[0][0];
+		var hf_entry = select_hf298_1(d);
 		return parse(hf_entry)[0];
 	})]).range([padding, w - padding]);
 
 	var yScale = d3.scale.linear().domain([0, d3.max(dataset, function(d) {
-		var hf_entry = d3.select(d).select('hf298_1')[0][0];
+		var hf_entry = select_hf298_1(d);
 		return parse(hf_entry)[1];
 	})]).range([h - padding, padding]);
 
 	var rScale = d3.scale.linear().domain([0, d3.max(dataset, function(d) {
-		var hf_entry = d3.select(d).select('hf298_1')[0][0];
+		var hf_entry = select_hf298_1(d);
 		return parse(hf_entry)[1];
 	})]).range([0, 5]);
 
@@ -27,49 +27,45 @@ function createScatterPlot(dataset) {
 	var svg = d3.select("#scatter").append("svg").attr("width", w).attr("height", h);
 
 	svg.selectAll("circle").data(dataset).enter().append("circle").attr("cx", function(d) {
-		var hf_entry = d3.select(d).select('hf298_1')[0][0];
+		var hf_entry = select_hf298_1(d);
 		return xScale(parse(hf_entry)[0]);
 	}).attr("cy", function(d) {
-		var hf_entry = d3.select(d).select('hf298_1')[0][0];
+		var hf_entry = select_hf298_1(d);
 		return yScale(parse(hf_entry)[1]);
 	}).attr("r", function(d) {
-		var hf_entry = d3.select(d).select('hf298_1')[0][0];
+		var hf_entry = select_hf298_1(d);
 		var yval = parse(hf_entry)[1];
 		return yval == 0 ? 0 : 2;
 		//return hf_entry == null ? 0: rScale(parseUncertainty(hf_entry));
 	});
-	;
-	/*
-	 svg.selectAll("text")
+	
+	
+	svg.selectAll("text")
 	 .data(dataset)
 	 .enter()
 	 .append("text")
 	 .text(function(d) {
-	 var hf_entry = d3.select(d).select('hf298_1')[0][0];
-	 var x = hf_entry == null ? '': xScale(parseHf(hf_entry)).toFixed(0),
-	 y = hf_entry == null ? '': yScale(parseUncertainty(hf_entry)).toFixed(0);
-	 if (x == '' || y == ''){
-	 return '';
-	 }
-	 else{
-	 return x + "," + y;
-	 }
+
+		var hf_entry = select_hf298_1(d);
+		var x = parse(hf_entry)[0].toFixed(0), y = parse(hf_entry)[1].toFixed(0);
+		if (x == '' || y == '') {
+			return '';
+		} else {
+			return '('+x + ',' + y+')';
+		}
 
 	 })
 	 .attr("x", function(d) {
-	 var hf_entry = d3.select(d).select('hf298_1')[0][0];
-	 var x = hf_entry == null ? 0: xScale(parseHf(hf_entry));
+	 var hf_entry = select_hf298_1(d);
+	 var x = xScale(parse(hf_entry)[0]);
 	 return x;
 	 })
 	 .attr("y", function(d) {
-	 var hf_entry = d3.select(d).select('hf298_1')[0][0];
-	 var y = hf_entry == null ? 0: yScale(parseUncertainty(hf_entry));
+	 var hf_entry = select_hf298_1(d);
+	 var y = yScale(parse(hf_entry)[1]);
 	 return y;
 	 })
-	 .attr("font-family", "sans-serif")
-	 .attr("font-size", "11px")
-	 .attr("fill", "red");
-	 */
+	 .attr('id', 'circle_label');
 
 	var xAxis = d3.svg.axis().scale(xScale).orient("bottom").ticks(5);
 	//Set rough # of ticks
@@ -87,6 +83,9 @@ function createScatterPlot(dataset) {
 
 }
 
+function select_hf298_1(species_entry){
+	return d3.select(species_entry).select('hf298_1')[0][0];
+}
 function parse(hf_entry) {
 	if (hf_entry == null) {
 		return [0, 0];
