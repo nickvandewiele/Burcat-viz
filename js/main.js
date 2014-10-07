696bbd76602c5cfac8d760bf2a6c525c01eb927d
Nick Vandewiele <nickvandewiele@gmail.com>
2014-10-07 11:21:15 -0400

    correct relative path of data/BURCAT_THR.xml

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-d3.xml("/data/BURCAT_THR.xml", function(xml) {
+d3.xml("data/BURCAT_THR.xml", function(xml) {
 	dataset = d3.select(xml).selectAll("specie")[0];
 	createBubbleChart();
 	createScatterPlot(dataset);
